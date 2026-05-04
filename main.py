1fa1824b5a554b036952fa3779a6d5d02f8faeae
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2026-05-04 16:42:29 +0300

    add color cycling for neopixel LED

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import machine, neopixel
 led = neopixel.NeoPixel(machine.Pin(38), 1)
 led[0] = (1, 1, 1)
 led.write()
+
+import time
+while True:
+    led[0] = (255, 255, 0) 
+    led.write()
+    time.sleep(1)
+    led[0] = (0, 255, 255) 
+    led.write()
+    time.sleep(1)
+    led[0] = (255, 0, 255) 
+    led.write()
+    time.sleep(1)
